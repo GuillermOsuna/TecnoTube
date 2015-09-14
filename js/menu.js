3fca4b24be0badda12b808b91faf7fe4fa847e1c
GuillermOsuna <gosuna79@gmail.com>
2015-09-13 23:01:47 -0600

    pre parallax

--- js/menu.js
+++ js/menu.js
@@ -17,8 +17,15 @@ function main(){
 			});
 		}
 	});
-};
+	};
 
+		/*function parallax(){
+		var prlx_lyr_1.style.top= -document.getElementsByTagName('gallery');
+		prlx_lyr_1.style.top= -(window.pageYOffset/4)+ 'px';
+
+	};
+	window.addEventListener("scroll", parallax, false;);	
+*/
 
 
 
